ad8c7fa8c33c27281066d0071807a7d134afd4c3
Saykillmeplz <andreysm1988@gmail.com>
2025-11-21 14:01:55 +0300

    123456

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,12 +4,12 @@ from playwright.sync_api import Page
 class MainPage:
     def __init__(self, page: Page):
         self.page = page
-        self.login = page.locator("//input[@name='login']")
+        self.user_login = page.locator("//input[@name='login']")
         self.password = page.locator("//input[@name='pass']")
         self.enter = page.locator("//button[text()='Войти']")
 
     def login(self):
         self.page.goto("https://dev.topklik.online/")
-        self.login.fill("tester@inzhenerka.tech")
+        self.user_login.fill("tester@inzhenerka.tech")
         self.password.fill("LetsTest!")
         self.enter.click()
