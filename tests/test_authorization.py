28b2f2ac9f46fff406dffe953f6162441503ef7a
Saykillmeplz <andreysm1988@gmail.com>
2025-11-21 15:13:19 +0300

    1

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,7 +1,9 @@
 from playwright.sync_api import Page, expect
 from pages.login_page import MainPage
+import allure
 
 
+@allure.title("Проверка логина")
 def test_login(page: Page):
     main_page = MainPage(page)
 
